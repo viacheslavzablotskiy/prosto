73ca9ad224d2d0cf7edcbbe8593a9e015678a2cc
viacheslavzablotskiy <zlava.mag@gmail.com>
2022-12-01 21:13:07 +0300

    "load"

--- docker/docker/settings.py
+++ docker/docker/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'docker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':os.environ.get('POSTGRES_DATABASE', 'logout_port'),
+        'NAME':os.environ.get('POSTGRES_DATABASE', 'postgres'),
         'USER':os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD':os.environ.get('POSTGRES_PASSWORD', '286226'),
         'HOST':os.environ.get('POSTGRES_HOST', '127.0.0.1'),
